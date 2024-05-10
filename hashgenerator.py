ab8125644f92d5502cd1ceedc1faa95d9e8754b8
Harshit Yadav <harshityadav@outlook.com>
2024-05-10 20:49:17 +0530

    User Authentication

---
+++ hashgenerator.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # This is a sample Python script.
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from streamlit_authenticator import Authenticate
+from yaml.loader import SafeLoader
+
+
 # Press Ctrl+F5 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -14,3 +20,30 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     st.write("Hello Incite User")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+with open('users.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+
+authenticator.login()
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
